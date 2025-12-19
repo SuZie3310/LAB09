04a35166e702fe595c27153191331caa341a0b83
SuZie3310 <Surapat5163@gmail.com>
2025-12-19 15:33:42 +0700

    1

--- lab9_2.cpp
+++ lab9_2.cpp
@@ -1,6 +1,22 @@
 #include<iostream>
 using namespace std;
 
+
+
+int printO(int rows, int cols){
+	if (rows <= 0 || cols <= 0) {
+		cout << "Invalid input\n";
+		return 0;
+	}
+	for (int i = 0; i < rows; i++) {
+		for (int j = 0; j < cols; j++) {
+			cout << "O";
+		}
+		cout << "\n";
+	}
+	return 0;
+}
+
 //Write the function printO() here
 
 int main(){
